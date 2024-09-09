184f5822a0aaf868b3a6306f2a92ef4d6aeacb5c
yogesh <yogesh81297@gmail.com>
2024-09-10 04:47:26 +0530

    Update .gitignore to ignore .firebase directory and remove .firebase from tracking and changed firebase config file

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBLEVVjObEagtW2-GzGRpzrGBcuMDarBHA",
-  authDomain: "netflixgpt-26f9f.firebaseapp.com",
-  projectId: "netflixgpt-26f9f",
-  storageBucket: "netflixgpt-26f9f.appspot.com",
-  messagingSenderId: "315470320128",
-  appId: "1:315470320128:web:7f368aa31dd703d1c83ee5",
-  measurementId: "G-TKBZ4E9BJP"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
